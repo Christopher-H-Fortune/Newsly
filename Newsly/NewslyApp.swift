18b3954d1db41fea2902643ee6cdf4d1eabbb055
Christopher Fortune <cfortune97@gmail.com>
2023-09-16 17:28:35 -0400

    Initial Commit

---
+++ Newsly/NewslyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewslyApp.swift
+//  Newsly
+//
+//  Created by Chris Fortune on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct NewslyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
